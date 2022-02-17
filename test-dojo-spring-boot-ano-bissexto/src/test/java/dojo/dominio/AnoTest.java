a2de33c4c6a853eac64ed3fcbd6b79948ff11810
CIANDT\alanjhone <alanjhone@ciandt.com>
2022-02-17 11:02:22 -0300

    adicionando cenario de divisao por 400

--- test-dojo-spring-boot-ano-bissexto/src/test/java/dojo/dominio/AnoTest.java
+++ test-dojo-spring-boot-ano-bissexto/src/test/java/dojo/dominio/AnoTest.java
@@ -2,6 +2,7 @@ package dojo.dominio;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AnoTest {
@@ -9,6 +10,18 @@ public class AnoTest {
     @Test
     public void test1() {
         boolean isBissexto = Ano.isBissexto(2020);
+        assertTrue(isBissexto);
+    }
+
+    @Test
+    public void test2() {
+        boolean isBissexto = Ano.isBissexto(2005);
+        assertFalse(isBissexto);
+    }
+
+    @Test
+    public void testAnoDivisivelPor400() {
+        boolean isBissexto = Ano.isBissexto(2400);
         assertTrue(isBissexto);
     }
 }
