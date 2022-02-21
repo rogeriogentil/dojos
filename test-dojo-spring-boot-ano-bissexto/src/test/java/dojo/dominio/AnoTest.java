dba9d07840c79f7c434d2f523c31e9ea32954d5c
camillamsantos <84816792+camillamsantos@users.noreply.github.com>
2022-02-21 10:38:16 -0300

    dojo testes

--- test-dojo-spring-boot-ano-bissexto/src/test/java/dojo/dominio/AnoTest.java
+++ test-dojo-spring-boot-ano-bissexto/src/test/java/dojo/dominio/AnoTest.java
@@ -2,6 +2,7 @@ package dojo.dominio;
 
 import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AnoTest {
@@ -10,5 +11,23 @@ public class AnoTest {
     public void test1() {
         boolean isBissexto = Ano.isBissexto(2020);
         assertTrue(isBissexto);
+    }
+
+    @Test
+    public void todoAnoDivisivelPor400(){
+       boolean isBissexto =  Ano.isBissexto(2100);
+       assertTrue(isBissexto);
+    }
+
+    @Test
+    public void todoAnoDivisivelPor100MasNaoPor400(){
+        boolean isBissexto =  Ano.isBissexto(1700);
+       assertFalse(isBissexto);
+    }
+
+    @Test
+    public void anoDivisivelPor4MasNaoPor100(){
+        boolean isBissexto = Ano.isBissexto(2008);
+        assertTrue (isBissexto);
     }
 }
